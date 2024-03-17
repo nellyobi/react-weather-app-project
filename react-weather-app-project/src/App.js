a239b65a856dc1bb12f5432f0117b72e691320fd
Nelly <nellyobijiaku@gmail.com>
2024-03-17 20:28:43 +0000

    Added the Github repo URL

--- react-weather-app-project/src/App.js
+++ react-weather-app-project/src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather App</div>
+    <div className="App"> 
+    
+    <h1> Weather App </h1>
+    <footer>
+      This project was coded by <a href="https://github.com/nellyobi" target="_blank"> Nelly </a>  and is {" "}
+    <a href="https://github.com/nellyobi/react-weather-app-project"
+    target='_blank'>
+open-sourced on Github
+
+    </a> 
+    </footer>
+    </div>
   );
 }
 
