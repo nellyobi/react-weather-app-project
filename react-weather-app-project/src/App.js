5c6aa7e0aba419632f98c24b201475d21dd32931
Nelly <nellyobijiaku@gmail.com>
2024-03-17 20:45:03 +0000

    Added bootstrap

--- react-weather-app-project/src/App.js
+++ react-weather-app-project/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App"> 
+    <div className="container">
     
     <h1> Weather App </h1>
     <footer>
@@ -14,6 +15,7 @@ open-sourced on Github
 
     </a> 
     </footer>
+    </div>
     </div>
   );
 }
