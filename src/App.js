3cd625d052a9c49e3d56f9ca1324d8705fc5fed4
Nelly <nellyobijiaku@gmail.com>
2024-03-21 17:04:38 +0000

    moved folders and corrected code lines

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App"> 
+    <div className="container">
+    
+    <h1> Weather App </h1>
+    <Weather defaultCity="London" />
+    <footer>
+      This project was coded by <a href="https://github.com/nellyobi"target="_blank" rel="noreferrer"> Nelly </a>  and is {" "}
+    <a href="https://github.com/nellyobi/react-weather-app-project"
+    target='_blank'>
+open-sourced on Github
+
+    </a> 
+    </footer>
+    </div>
+    </div>
+  );
+}
+
+
